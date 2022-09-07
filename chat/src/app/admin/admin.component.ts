a9df0ed22269519ee1a799aad5112824e961b63f
Toshimitsu Ota <89127228+toshimitsu-o@users.noreply.github.com>
2022-09-07 14:48:21 +1000

    Admin/Users section added. Delete user func added.

--- chat/src/app/admin/admin.component.ts
+++ chat/src/app/admin/admin.component.ts
@@ -10,11 +10,13 @@ import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
 export class AdminComponent implements OnInit {
 
   user: any;
+  users = [{username: "user1"}, {username: "user2"}, {username: "user3"}]; 
   groups = [{id: "g1", name:"Group 1"}, {id: "g2", name:"Group 2"}, {id: "g3", name:"Group 3"}];
   channels = [{id: "c1", name:"Channel 1", gid: "g1"}, {id: "c2", name:"Channel 2", gid: "g1"}, {id: "c3", name:"Channel 3",  gid: "g2"}];
   resultChannels: any[] = [];
   groupMembers = [{username: "user1", gid: "g1"}, {username: "user2", gid: "g2"}, {username: "user3", gid: "g3"}];
-  resultGroupMembers: any[] = [];
+  channelMembers = [{username: "user1", cid: "c1"}, {username: "user2", cid: "c2"}, {username: "user3", cid: "c3"}];
+  resultMembers: any[] = [];
   selectedGroup = "";
   public isCollapsed = true;
   public isCollapsedC = true;
@@ -45,18 +47,56 @@ export class AdminComponent implements OnInit {
     this.groups = this.groups.filter(g => g.id != id)
   }
 
+  removeChannel(id: string): void {
+    this.channels = this.channels.filter(c => c.id != id)
+  }
+
+  // Open a modal to show a list of channels
   showChannels(content: any, id: string): void {
     this.selectedGroup = id;
     this.modalService.open(content, { size: 'lg' });
   }
 
-  showUsers(content: any, id: string): void {
-    this.resultGroupMembers = this.groupMembers.filter(g => g.gid == id);
+  // Open a modal to show a list of users
+  showMembers(content: any, filter: string, id: string): void {
+    let targetMembers;
+    if (filter == "group") { // Show only from the group
+      targetMembers = this.groupMembers.filter(m => m.gid == id);
+    } else if (filter == "channel") { // Show only from the channel
+      targetMembers = this.channelMembers.filter(m => m.cid == id);
+    } else { // Show all
+      targetMembers = this.users;
+    }
+    this.resultMembers = targetMembers;
     this.modalService.open(content, { size: 'lg' });
   }
 
-  removeChannel(id: string): void {
-    this.channels = this.channels.filter(c => c.id != id)
+
+  createUser(username: string, email: string, password: string): void {
+    if (this.users.find(u => u.username == username)) { // Check if already exist
+      alert("Username already taken!");
+    } else {
+      this.users.push({username: username});
+    }
+  }
+
+  // Delete one user
+  deleteUser(user: any) {
+    if (this.user.role == 'superAdmin') {
+      this.users = this.users.filter(u => u.username != user);
+    } else {
+      alert("You don't have a permission for this action.");
+    }
+  }
+
+  // Delete all users except the current user
+  deleteAllUsers() {
+    if (this.user.role == 'superAdmin') {
+      this.users = this.users.filter(u => u.username == this.user.username);
+    } else {
+      alert("You don't have a permission for this action.");
+    }
+    
   }
 
 }
